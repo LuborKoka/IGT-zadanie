c8aa82214d2cdb72f647bd1cd57347f70edf493c
LuborKoka <koka.lubor87@gmail.com>
2025-01-19 19:36:48 +0100

    upload image on product create

--- clothing_dashboard/lib/clothing_dashboard_web/controllers/product_controller.ex
+++ clothing_dashboard/lib/clothing_dashboard_web/controllers/product_controller.ex
@@ -8,12 +8,25 @@ defmodule ClothingDashboardWeb.ProductController do
             {number, _} -> number
             :error -> nil
         end
+
+        photo_path = case params["photo"] do
+            %Plug.Upload{} = upload ->
+                extension = Path.extname(upload.filename)
+                filename = "#{Ecto.UUID.generate()}#{extension}"
+
+                dest = Path.join("priv/static/images/product_images", filename)
+
+                File.cp!(upload.path, dest)
+                
+                "/images/product_images/#{filename}"
+        end
+
         product_params = %{
             title: params["title"],
             stock: String.to_integer(params["stock"]),
             price: price,
             description: params["description"],
-            photo: "placeholder.jpg",
+            photo: photo_path,
             category: params["category"],
         }
     
