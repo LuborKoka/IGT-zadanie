e75b5a3be6cd11feeee883ee3822d951b2349fa7
LuborKoka <koka.lubor87@gmail.com>
2025-01-20 19:44:01 +0100

    security: validate month param

--- clothing_dashboard/lib/clothing_dashboard_web/controllers/page_controller.ex
+++ clothing_dashboard/lib/clothing_dashboard_web/controllers/page_controller.ex
@@ -18,9 +18,16 @@ defmodule ClothingDashboardWeb.PageController do
       nil -> [nil, nil]
       encoded_month -> 
         decoded = URI.decode(encoded_month)
-        [month, year] = String.split(decoded, ",")
-        year = String.to_integer(year)
-        [month, year]
+        with(
+          [month, year] <- String.split(decoded, ",") |> validate_parts(),
+          true <- validate_month(month),
+          {:ok, year_str} <- validate_year(year),
+          {year_int, _} <- Integer.parse(year_str)
+        ) do
+          [String.capitalize(month), year_int]
+        else
+          _ -> [nil, nil]
+        end
     end
 
     stock = ProductService.get_total_stock()
@@ -39,4 +46,23 @@ defmodule ClothingDashboardWeb.PageController do
     )
   end
 
+  defp validate_parts(parts) when length(parts) == 2, do: parts
+  defp validate_parts(_), do: nil
+
+
+  defp validate_month(month) do
+    valid_months = [
+      "january", "february", "march", "april", "may", "june",
+      "july", "august", "september", "october", "november", "december"
+    ]
+    String.downcase(month) in valid_months
+  end
+
+
+  defp validate_year(year) do
+    case Integer.parse(year) do
+      {_year_int, ""} -> {:ok, year}
+      _ -> :error
+    end
+  end
 end
