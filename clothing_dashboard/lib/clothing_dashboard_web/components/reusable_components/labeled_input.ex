5e4a2af2d8d4f29b73327aa12763ca022ad2361f
LuborKoka <koka.lubor87@gmail.com>
2025-01-21 12:35:16 +0100

    UI: stick to one color scheme

--- clothing_dashboard/lib/clothing_dashboard_web/components/reusable_components/labeled_input.ex
+++ clothing_dashboard/lib/clothing_dashboard_web/components/reusable_components/labeled_input.ex
@@ -13,9 +13,9 @@ defmodule ClothingDashboard.LabeledInput do
         ~H"""
             <div class={"relative w-full group #{@wrapper_classes}"}>
                 <%= if @is_textarea do %>
-                    <textarea  id={@id}  name={String.downcase(@name)}  required  class="peer w-full bg-transparent px-5 py-4 text-xl border rounded outline-none transition-shadow duration-300 hover:shadow-lg focus:shadow-lg valid:shadow-lg" />
+                    <textarea  id={@id}  name={String.downcase(@name)}  required  class="peer w-full bg-transparent px-5 py-4 text-xl border rounded outline-none transition-shadow duration-300 hover:shadow-lg focus:shadow-lg focus:border-green-500 focus:ring-green-500 valid:shadow-lg valid:border-green-500 valid:ring-green-500" />
                 <% else %>
-                    <input id={@id} name={String.downcase(@name)} min={@min} type={@type} required class="peer w-full bg-transparent px-5 py-4 text-xl border rounded outline-none transition-shadow duration-300 hover:shadow-lg focus:shadow-lg valid:shadow-lg"/>
+                    <input id={@id} name={String.downcase(@name)} min={@min} type={@type} required class="peer w-full bg-transparent px-5 py-4 text-xl border rounded outline-none transition-shadow duration-300 hover:shadow-lg focus:shadow-lg focus:border-green-500 focus:ring-green-500 valid:shadow-lg valid:border-green-500 valid:ring-green-500"/>
                 <% end %>
                 <label for={@id} class="absolute left-4 top-4 text-xl pointer-events-none bg-white transition-all duration-300 peer-focus:-translate-y-full peer-focus:text-base peer-valid:-translate-y-full peer-valid:text-base peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-xl">
                     <%= @placeholder %>
